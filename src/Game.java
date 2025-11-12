a539170ecd9c7bf583788fa1ec22939135d4111d
Vadim Dudar <haskervadim228@gmail.com>
2025-11-12 15:07:14 +0200

    set racket to follow mouse and add main constants

--- src/Game.java
+++ src/Game.java
@@ -1,7 +1,33 @@
+import acm.graphics.*;
 import acm.program.GraphicsProgram;
 
-public class Game extends GraphicsProgram {
-    public void run(){
+import java.awt.*;
+import java.awt.event.MouseEvent;
 
+public class Game extends GraphicsProgram {
+
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = 800;
+
+    private static final int BRiCK_WIDTH = 50;
+    private static final int BRiCK_HEIGHT = 20;
+
+    private double w, h;
+    private GRect racket;
+
+    public void run(){
+        setSize(WIDTH, HEIGHT);
+
+        w = getWidth(); h = getHeight();
+        racket = new GRect(w-BRiCK_WIDTH*0.75, h-70, BRiCK_WIDTH*1.5, BRiCK_HEIGHT);
+        racket.setFilled(true);
+        racket.setFillColor(Color.BLACK);
+        add(racket);
+
+        addMouseListeners();
+    }
+
+    public void mouseMoved(MouseEvent e) {
+        racket.setLocation(e.getX()-BRiCK_WIDTH*0.75, racket.getY());
     }
 }
