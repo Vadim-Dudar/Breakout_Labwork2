04b96ff885f896ba0d55a2e0bf8b0a77a4ca3065
Vadim Dudar <haskervadim228@gmail.com>
2025-11-12 16:35:16 +0200

    add playSound and sounds for that will be used for game

--- src/Game.java
+++ src/Game.java
@@ -4,6 +4,9 @@ import acm.program.GraphicsProgram;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 
+import java.applet.Applet;
+import java.applet.AudioClip;
+
 public class Game extends GraphicsProgram {
 
     private static final int WIDTH = 800;
@@ -24,10 +27,35 @@ public class Game extends GraphicsProgram {
         racket.setFillColor(Color.BLACK);
         add(racket);
 
+        playSound("victory");
+
         addMouseListeners();
     }
 
     public void mouseMoved(MouseEvent e) {
         racket.setLocation(e.getX()-BRiCK_WIDTH*0.75, racket.getY());
+    }
+
+    /**
+     * Play a WAV sound file located in the user's downloads folder via
+     * the Applet AudioClip API. The filename base (without .wav)
+     * is provided as `soundName`.
+     *
+     * Existing sound in project:
+     *  - "victory" - Victory sound
+     *  - "bounce" - ball bounce sound
+     *  - "brick" - break brick sound
+     *  - "build" - sound of brick building
+     *
+     * @param soundName base filename of the WAV
+     */
+    private void playSound(String soundName) {
+        try {
+            java.net.URL url = new java.io.File("media\\"+soundName+".wav").toURI().toURL();
+            AudioClip sound = Applet.newAudioClip(url);
+            sound.play();
+        } catch (java.net.MalformedURLException e) {
+            System.err.println("Failed to load bomb sound: " + e.getMessage());
+        }
     }
 }
